f68d507a0a5480e208423ac7b07e2a3c22c5e54b
Tech2705 <160301845+Tech2705@users.noreply.github.com>
2026-02-06 22:37:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", ""))
+    API_ID = int(os.environ.get("API_ID", "0"))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    DB_CHANNEL_ID = int(os.environ.get("DB_CHANNEL_ID", "-100"))
-    UPDATES_CHANNEL_ID = int(os.environ.get("UPDATES_CHANNEL_ID", "-100"))
+    DB_CHANNEL_ID = int(os.environ.get("DB_CHANNEL_ID", "0"))
+    UPDATES_CHANNEL_ID = int(os.environ.get("UPDATES_CHANNEL_ID", "0"))
